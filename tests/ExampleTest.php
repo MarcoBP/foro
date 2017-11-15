8753d1f5757df13b15f56edeefb76bcffe0e5eff
Marco Barreto <admysis@hotmail.com>
2017-11-14 20:30:01 -0500

    First commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $user = factory(App\User::class)->create([
+            'name' => 'Marco Barreto',
+            'email' => 'admysis@hotmail.com',
+        ]);
+
+        $this->actingAs($user, 'api');
+
+        $this->visit('api/user')
+             ->see('Marco Barreto')
+             ->see('admysis@hotmail.com');
+    }
+}
