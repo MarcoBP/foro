8ef13d832d9101af333b8ac8a0a301f7746f32be
Marco Barreto <admysis@hotmail.com>
2017-12-06 22:38:39 -0500

    Curso 15 Uso de model factories para crear registros relacionados

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -25,8 +25,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
 $factory->define(App\Post::class, function (Faker\Generator $faker) {
     return [
-      'title' => $faker->sentence,
-      'content' => $faker->paragraph,
-      'pending' => $faker->boolean()
+        'title' => $faker->sentence,
+        'content' => $faker->paragraph,
+        'pending' => $faker->boolean(),
+        'user_id' => function() {
+            return factory(\App\User::class)->create()->id;
+        },
     ];
 });
--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -1,10 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
-
-class ShowPostTest extends TestCase
+class ShowPostTest extends FeatureTestCase
 {
     function test_a_user_can_see_the_post_details()
     {
@@ -13,19 +9,69 @@ class ShowPostTest extends TestCase
             'name' => 'Marco Barreto',
         ]);
 
-        $post = factory(\App\Post::class)->make([
+        $post = $this->createPost([
             'title' => 'Este es el título del post',
-            'content' => 'Este es el contenido del post'
+            'content' => 'Este es el contenido del post',
+            'user_id' => $user->id,
+        ]);
+
+        // Imprime en un array una lista de los usuarios del sistema, y el user_id del Post
+        // dd(\App\User::all()->toArray(), $post->user_id);
+
+        //When
+
+        // Cambiamos esta ruta larga por la nueva manejada por el atributo: url
+        // $this->visit(route('posts.show', [$post->id, $post->slug]))
+
+        // Estas instrucciones temporales se deben deshabilitar al ejecutar nuevamene la prueba completa
+        // dd($post->url);
+
+        $this->visit($post->url)
+            ->seeInElement('h1', $post->title)
+            ->see($post->content)
+            ->see('Marco Barreto');
+    }
+
+    function test_old_urls_are_redirected(){
+
+        // Having
+        $post = $this->createPost([
+            'title' => 'Old title',
+        ]);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->seePageIs($post->url);
+
+    }
+
+    // Se deshabilita esta prueba para generar una nueva prueba con otra funcionalidad
+    /*
+    function test_post_url_with_wrong_slugs_still_work()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
         ]);
 
         $user->posts()->save($post);
 
-        //When
+        $url = $post->url;
 
-        $this->visit(route('posts.show', $post))
-            ->seeInElement('h1', $post->title)
-            ->see($post->content)
-            ->see($user->name);
+        $post->update(['title' => 'New title']);
+
+        // dd($url);
+
+        $this->visit($url)
+            ->assertResponseOk()
+            ->see('New title');
     }
+    */
+
 
 }
