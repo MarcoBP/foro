f588f337668db50f8488f5ff8be31def4c3934b9
Marco Barreto <admysis@hotmail.com>
2017-12-11 22:12:19 -0500

    Curso 17 Paginar registros con TDD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,15 @@ use App\Post;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $posts = Post::orderBy('created_at','DESC')->paginate();
+
+        // dd($posts->pluck('created_at')->toArray());
+
+        return view('posts.index', \compact('posts'));
+    }
+
     public function show(Post $post, $slug)
     {
         // Se deshabilita esta prueba para generar una nueva prueba con otra funcionalidad
